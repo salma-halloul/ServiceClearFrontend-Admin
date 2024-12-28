f9fe9eedc8b533d282f48c56e8411d57281752b2
Salma Halloul <salmahalloul52@gmail.com>
2024-12-28 16:52:46 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  compress: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
